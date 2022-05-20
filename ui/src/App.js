eb9ad0a0fa0f40f835bf43ccff0dfa4ccdef30d7
Mohammed Refaat <Mohrefaat98@gmail.com>
2022-05-20 04:02:48 +0200

    Edit the main page

--- ui/src/App.js
+++ ui/src/App.js
@@ -3,21 +3,22 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App App-header">
+      <h3>
+        URL Shortener
+      </h3>
+      <form className="form">
+        <input type="text" id="link" />
+        <input type="button" for="link" id="enter" value="Shorten"/>
+      </form>
+      <a
+        className="App-link"
+        href="https://reactjs.org"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Learn React
+      </a>
     </div>
   );
 }
